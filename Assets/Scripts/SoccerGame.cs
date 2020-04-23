a55f499722c011f08efab1dcfe275ba938bce79f
Rafael Acuña <clubrafa@yahoo.com.mx>
2020-04-23 16:29:53 -0500

    Soccer Updates
    Scene updates, Scripts

--- Assets/Scripts/SoccerGame.cs
+++ Assets/Scripts/SoccerGame.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class SoccerGame : MonoBehaviour
 {
-    public GameObject ball;
+    public GameObject ball, rA, rB, rC, rD, rE, rF;
     private Vector3 initialPosition;
+    private Vector3[] referencePoints;
     private Rigidbody ballRigidbody;
+    private bool kicked;
 
     // Start is called before the first frame update
     void Start()
     {
+        kicked = false;
         initialPosition = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+
         ballRigidbody = (Rigidbody)ball.GetComponent("Rigidbody");
+
+        referencePoints = new Vector3[6];
+        referencePoints[0] = rA.transform.position;
+        referencePoints[1] = rB.transform.position;
+        referencePoints[2] = rC.transform.position;
+        referencePoints[3] = rD.transform.position;
+        referencePoints[4] = rE.transform.position;
+        referencePoints[5] = rF.transform.position;
     }
 
     // Update is called once per frame
@@ -20,11 +32,21 @@ public class SoccerGame : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            ballRigidbody.AddForce(initialPosition, ForceMode.Impulse);
+            kicked = true;
+            Vector3 kickDirection = (referencePoints[Random.Range(0, 5)] - ball.transform.position).normalized;
+            ballRigidbody.AddForce(kickDirection * (float)30.0, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("m"))
         {
-            ball.transform.position = initialPosition;
+            stopBall();
         }
+    }
+
+    public void stopBall()
+    {
+        kicked = false;
+        ballRigidbody.velocity = Vector3.zero;
+        ballRigidbody.angularVelocity = Vector3.zero;
+        ball.transform.position = initialPosition;
     }
 }
---
+++ Assets/Scripts/SoccerGoalCheck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerGoalCheck : MonoBehaviour
+{
+    public SoccerGame gameScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        gameScript.stopBall();
+    }
+}

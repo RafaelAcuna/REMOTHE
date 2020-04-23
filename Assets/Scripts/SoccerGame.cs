930b1316332c7b2533b7d2f6031ee58fbfe55f5a
Rafael Acuña <clubrafa@yahoo.com.mx>
2020-04-23 11:42:52 -0500

    Keeper Progress
    Materials, Models, Textures and Plugins

---
+++ Assets/Scripts/SoccerGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerGame : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 initialPosition;
+    private Rigidbody ballRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+        ballRigidbody = (Rigidbody)ball.GetComponent("Rigidbody");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            ballRigidbody.AddForce(initialPosition, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            ball.transform.position = initialPosition;
+        }
+    }
+}
